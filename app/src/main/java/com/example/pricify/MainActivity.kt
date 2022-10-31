f9d1ec7546cb5ba9fb8f2064d334024ce2942bf4
Grant Martin <grantmartin2002@gmail.com>
2022-10-30 22:56:26 -0500

    added auto login

--- app/src/main/java/com/example/pricify/MainActivity.kt
+++ app/src/main/java/com/example/pricify/MainActivity.kt
@@ -4,11 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pricify.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +22,11 @@ class MainActivity : AppCompatActivity() {
 
         // showing the back button in action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        if(FirebaseAuth.getInstance().currentUser!=null){
+            launchHome();
+            return;
+        }
 
 
     }
