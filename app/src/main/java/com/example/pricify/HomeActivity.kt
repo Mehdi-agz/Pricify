982f1f30a1bbe1fe363b720b7f9182bdae6bf27a
Grant Martin <grantmartin2002@gmail.com>
2022-10-30 21:39:49 -0500

    added logout with confirmation

--- app/src/main/java/com/example/pricify/HomeActivity.kt
+++ app/src/main/java/com/example/pricify/HomeActivity.kt
@@ -1,15 +1,19 @@
 package com.example.pricify
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.example.pricify.databinding.ActivityHomeBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityHomeBinding
-    private lateinit var listIntent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,51 +25,56 @@ class HomeActivity : AppCompatActivity(){
         binding.logoutBtn.setOnClickListener { logOut() }
         binding.settingsBtn.setOnClickListener { lunchSettings() }
 
-//        binding.logoutBtn.setOnClickListener { lunchLogOut() }
-
         // Enable up button for backward navigation
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-//    private fun logoutConfirmationAlert(): AlertDialog? {
-//        val builder = AlertDialog.Builder(this)
-//        builder.setTitle("Logout Confirmation")
-//        builder.setMessage("Are you sure you want to logout?")
-////        builder.setPositiveButton("Yes", DialogInterface.OnClickListener()
-////
-////        builder.setPositiveButton("Yes") { dialog, which ->
-////            Toast.makeText(applicationContext,
-////                android.R.string.ok, Toast.LENGTH_SHORT).show()
-////        }
-////
-////        builder.setNegativeButton(android.R.string.cancel) { dialog, which ->
-////            Toast.makeText(applicationContext,
-////                android.R.string.cancel, Toast.LENGTH_SHORT).show()
-////        }
-////
-////        return builder.create()
-//    }
-
-    private fun logOut(){
-
+    private fun launchMain() {
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
-    private fun logOutConfirmed() {
+    fun logOutConfirmed() {
+        Toast.makeText(applicationContext,
+            "Logging out!", Toast.LENGTH_SHORT).show()
         FirebaseAuth.getInstance().signOut();
         launchMain();
     }
 
-        private fun launchMain(){
-        listIntent = Intent(this, MainActivity::class.java)
-        startActivity(listIntent)
+
+    inner class ConfirmLogoutOnClickListener() : DialogInterface.OnClickListener {
+        override fun onClick(p0: DialogInterface?, p1: Int) {
+            logOutConfirmed()
+        }
+
+
     }
+
+    private fun logoutConfirmationAlert(): AlertDialog? {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Logout Confirmation")
+        builder.setMessage("Are you sure you want to logout?")
+        builder.setPositiveButton("Confirm", ConfirmLogoutOnClickListener())
+
+        builder.setNegativeButton(android.R.string.cancel) { dialog, which ->
+
+        }
+
+        return builder.create()
+    }
+
+    private fun logOut(){
+        logoutConfirmationAlert()!!.show()
+    }
+
+
     private fun lunchSettings() {
-        listIntent = Intent(this, SettingsActivity::class.java)
-        startActivity(listIntent)
+        var intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
     }
 
     private fun launchWishLists() {
-        listIntent = Intent(this, WishListsActivity::class.java)
-        startActivity(listIntent)
+        var intent = Intent(this, WishListsActivity::class.java)
+        startActivity(intent)
     }
 }
