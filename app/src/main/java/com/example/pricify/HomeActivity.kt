b235cb1e13d85ee6f1454eccc24eb8933b89b120
Grant Martin <grantmartin2002@gmail.com>
2022-10-30 20:53:46 -0500

    basic logout functionality in the works

--- app/src/main/java/com/example/pricify/HomeActivity.kt
+++ app/src/main/java/com/example/pricify/HomeActivity.kt
@@ -1,9 +1,13 @@
 package com.example.pricify
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pricify.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity(){
 
@@ -17,22 +21,44 @@ class HomeActivity : AppCompatActivity(){
 
         binding.wishlistsBtn.setOnClickListener { launchWishLists() }
 
-//        binding.settingsBtn.setOnClickListener { lunchSettings() }
-
-//        binding.logoutBtn.setOnClickListener { lunchLogOut() }
+        binding.logoutBtn.setOnClickListener { logOut() }
 
         // Enable up button for backward navigation
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-//    private fun lunchLogOut() {
-//        TODO("Not yet implemented")
+//    private fun logoutConfirmationAlert(): AlertDialog? {
+//        val builder = AlertDialog.Builder(this)
+//        builder.setTitle("Logout Confirmation")
+//        builder.setMessage("Are you sure you want to logout?")
+////        builder.setPositiveButton("Yes", DialogInterface.OnClickListener()
+////
+////        builder.setPositiveButton("Yes") { dialog, which ->
+////            Toast.makeText(applicationContext,
+////                android.R.string.ok, Toast.LENGTH_SHORT).show()
+////        }
+////
+////        builder.setNegativeButton(android.R.string.cancel) { dialog, which ->
+////            Toast.makeText(applicationContext,
+////                android.R.string.cancel, Toast.LENGTH_SHORT).show()
+////        }
+////
+////        return builder.create()
 //    }
 
-//    private fun lunchSettings() {
-//        listIntent = Intent(this, SettingsActivity::class.java)
-//        startActivity(listIntent)
-//    }
+    private fun logOut(){
+
+    }
+
+    private fun logOutConfirmed() {
+        FirebaseAuth.getInstance().signOut();
+        launchMain();
+    }
+
+        private fun launchMain(){
+        listIntent = Intent(this, MainActivity::class.java)
+        startActivity(listIntent)
+    }
 
     private fun launchWishLists() {
         listIntent = Intent(this, WishListsActivity::class.java)
