6881a0f82d6f9c40f29ccd4935d23e1d0c97819f
Grant Martin <grantmartin2002@gmail.com>
2022-10-30 20:55:33 -0500

    merge

--- app/src/main/java/com/example/pricify/HomeActivity.kt
+++ app/src/main/java/com/example/pricify/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pricify.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity(){
 
