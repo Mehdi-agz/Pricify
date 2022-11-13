7838470509260a5c9009bdc65414d244b63f3136
Mehdi Asgharzadeh <49043600+mr-programmerr@users.noreply.github.com>
2022-11-13 15:53:56 -0600

    added checkbox functionality

--- app/src/main/java/com/example/pricify/AddItemActivity.kt
+++ app/src/main/java/com/example/pricify/AddItemActivity.kt
@@ -2,8 +2,11 @@ package com.example.pricify
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pricify.databinding.ActivityAddItemBinding
+import android.widget.CheckBox
+
 
 class AddItemActivity : AppCompatActivity() {
 
@@ -16,6 +19,18 @@ class AddItemActivity : AppCompatActivity() {
 
         binding = ActivityAddItemBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.itemPriceDrop.visibility = View.GONE
+
+        binding.checkbox.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked) {
+                binding.itemPriceDrop.visibility = View.VISIBLE
+//                binding.priceDropText.visibility = View.VISIBLE
+            } else {
+                binding.itemPriceDrop.visibility = View.GONE
+//                binding.priceDropText.visibility = View.GONE
+            }
+        }
 
         // showing the back button in action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
