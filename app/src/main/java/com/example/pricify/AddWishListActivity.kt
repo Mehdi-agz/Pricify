9616e2092e0989c54da55cf93c57ef61d5288d6e
Grant Martin <grantmartin2002@gmail.com>
2022-11-13 23:28:01 -0600

    added wishlist add into database

--- app/src/main/java/com/example/pricify/AddWishListActivity.kt
+++ app/src/main/java/com/example/pricify/AddWishListActivity.kt
@@ -2,8 +2,18 @@ package com.example.pricify
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pricify.databinding.ActivityAddWishlistBinding
+import com.example.pricify.model.Item
+import com.example.pricify.model.Wishlist
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class AddWishListActivity : AppCompatActivity() {
 
@@ -16,9 +26,63 @@ class AddWishListActivity : AppCompatActivity() {
 
         binding = ActivityAddWishlistBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.saveBtn.setOnClickListener { addWishlist() }
 
         // showing the back button in action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    private fun addWishlist() {
+        var name:String = binding.nameInput.text.toString().trim()
+
+        if(name.isEmpty()){
+            binding.nameInput.error = "Wishlist name is required!";
+            binding.nameInput.requestFocus();
+            return;
+        }
+        val userWishlistsRef = FirebaseDatabase.getInstance().getReference("Wishlists").child(FirebaseAuth.getInstance().currentUser!!.uid).ref
+        val context = this
+        userWishlistsRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.hasChild(name)) {
+                    binding.nameInput.error = "Wishlist name already exists! Enter a unique identifier!";
+                    binding.nameInput.requestFocus();
+                    return;
+                }else{
+                    binding.progressBar.visibility = View.VISIBLE
+                    var wishList = Wishlist(binding.nameInput.text.toString(),listOf<Item>())
+                    val newWishlist = FirebaseDatabase.getInstance().getReference("Wishlists").child(FirebaseAuth.getInstance().currentUser!!.uid).child(wishList.name)
+                    newWishlist.setValue(wishList).addOnCompleteListener{ task->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                context,
+                                "Wishlist successfully created!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            binding.progressBar.visibility = View.GONE
+                            launchWishlists()
+                        } else {
+                            Toast.makeText(
+                                context,
+                                task.exception?.message ?: "Exception without message",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            binding.progressBar.visibility = View.GONE
+                        }
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
+
+
+    }
+
+    private fun launchWishlists(){
+        var intent = Intent(this, WishListsActivity::class.java)
+        startActivity(intent)
+    }
 }
